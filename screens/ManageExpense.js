31cc0b3f82d3d23b051e2b58a5d259c9dc1f2cd1
ohwonsuk <wsoh51@gmail.com>
2024-03-28 00:09:56 +0900

    Manage Expense 버튼 만들기

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function MangeExpense({ route, navigation }) {
   // ? 는 파라미터로 넘어오는 객체가 있거나 없거나 적용하기 위함
@@ -12,7 +15,58 @@ function MangeExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>MangeExpense Screen</Text>;
+  function deleteExpenseHandler() {}
+
+  function cancelHandler() {}
+
+  function confirmHandler() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default MangeExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
